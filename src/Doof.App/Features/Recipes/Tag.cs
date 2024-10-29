ae678bf0d8be9377cb9901847c7584d35092ab97
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-10-29 11:22:51 +0100

    Seed Tag.cs

--- src/Doof.App/Features/Recipes/Tag.cs
+++ src/Doof.App/Features/Recipes/Tag.cs
@@ -7,13 +7,34 @@ public class Tag
 {
     public int Id { get; set; }
 
+    public required string UnicodeEmoji { get; set; }
+
     public ICollection<TagTranslation> Translations { get; set; } = [];
+
+    public ICollection<Recipe> Recipes { get; set; } = [];
 
     public static void Configure(ModelBuilder builder)
     {
-        builder.Entity<Tag>(e =>
+        builder.Entity<Tag>(t =>
         {
-            e.HasKey(t => t.Id);
+            t.HasKey(t => t.Id);
+
+            t.Property(tt => tt.UnicodeEmoji).IsRequired().HasMaxLength(9);
+
+            t.HasMany(tt => tt.Translations)
+                .WithOne(tt => tt.Tag)
+                .HasForeignKey(tt => tt.TagId);
         });
+    }
+
+    public static void Seed(ModelBuilder builder)
+    {
+        builder.Entity<Tag>().HasData(
+            new Tag
+            {
+                Id = 1,
+                UnicodeEmoji = "🍔"
+            }
+        );
     }
 }
