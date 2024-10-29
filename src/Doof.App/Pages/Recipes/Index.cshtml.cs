5290b74fed06f855b41a56a091093f817cea4642
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-10-29 11:21:23 +0100

    Tested so that it worked with the new data(it did)

--- src/Doof.App/Pages/Recipes/Index.cshtml.cs
+++ src/Doof.App/Pages/Recipes/Index.cshtml.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Doof.App.Data;
+using Doof.App.Features.Recipes;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Doof.App.Pages.Recipes;
 
 public class Recipes : PageModel
 {
-    public void OnGet()
+    private readonly ApplicationDbContext _context;
+
+    public Recipes(ApplicationDbContext context)
     {
+        _context = context;
+    }
+
+    public List<Recipe> Recipess { get; set; }
+
+    public async Task OnGetAsync()
+    {
+        Recipess = await _context.Recipes.ToListAsync();
 
     }
 }
